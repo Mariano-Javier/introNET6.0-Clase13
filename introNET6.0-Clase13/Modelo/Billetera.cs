49745eaa6d47a9236a570cd776c4cb541fd6d8ea
Mariano-Javier <mariano.javier.choqui@gmail.com>
2022-06-10 19:26:40 -0300

    Add project files.

---
+++ introNET6.0-Clase13/Modelo/Billetera.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace introNET6._0_Clase13.Modelo
+{
+    internal class Billetera
+    {
+        public int BilletesDe10 { get; set; }
+        public int BilletesDe20 { get; set; }
+        public int BilletesDe50 { get; set; }
+        public int BilletesDe100 { get; set; }
+        public int BilletesDe200 { get; set; }
+        public int BilletesDe500 { get; set; }
+        public int BilletesDe1000 { get; set; }
+
+
+        public decimal TotalEnBilletera()
+        {
+           return BilletesDe10 * 10 + BilletesDe20 * 20 + BilletesDe50 * 50 + BilletesDe100 * 100 + BilletesDe200 * 200 + BilletesDe500 * 500 + BilletesDe1000 * 1000;
+        }
+
+        public decimal TotalEnBilletera(int billetesDe10, int billetesDe20, int billetesDe50, int billetesDe100, int billetesDe200, int billetesDe500, int billetesDe1000)
+        {
+            return billetesDe10 * 10 + billetesDe20 * 20 + billetesDe50 * 50 + billetesDe100 * 100 + billetesDe200 * 200 + billetesDe500 * 500 + billetesDe1000 * 1000;
+        }
+
+        public void LimpiarBilletera()
+        {
+            BilletesDe10 = 0;
+            BilletesDe20 = 0;
+            BilletesDe50 = 0;
+            BilletesDe100 = 0;
+            BilletesDe200 = 0;
+            BilletesDe500 = 0;
+            BilletesDe1000 = 0;
+        }
+    }        
+}
---
+++ introNET6.0-Clase13/Program.cs
@@ -0,0 +1,63 @@
+﻿using introNET6._0_Clase13.Modelo;
+
+var billetera1 = new Billetera()
+{
+    BilletesDe10 = 10,
+    BilletesDe20 = 5,
+    BilletesDe50 = 2,
+    BilletesDe100 = 2,
+    BilletesDe200 = 3,
+    BilletesDe500 = 1,
+    BilletesDe1000 = 1,
+};
+
+var billetera2 = new Billetera()
+{
+    BilletesDe10 = 20,
+    BilletesDe20 = 10,
+    BilletesDe50 = 4,
+    BilletesDe100 = 1,
+    BilletesDe200 = 1,
+    BilletesDe500 = 2,
+    BilletesDe1000 = 3,
+};
+
+Console.WriteLine("--------Valores iniciales de las billeteras--------");
+
+//Usando los datos de billetera1.
+Console.WriteLine($"En la billetera 1 hay: {billetera1.TotalEnBilletera()} pesos");
+
+//Usando los datos de billetera2.
+Console.WriteLine($"En la billetera 2 hay: {billetera2.TotalEnBilletera()} pesos");
+
+static Billetera combinar(Billetera billetera01, Billetera billetera02)
+{
+    var billeteraNueva = new Billetera()
+    {
+        BilletesDe10 = billetera01.BilletesDe10 + billetera02.BilletesDe10,
+        BilletesDe20 = billetera01.BilletesDe20 + billetera02.BilletesDe20,
+        BilletesDe50 = billetera01.BilletesDe50 + billetera02.BilletesDe50,
+        BilletesDe100 = billetera01.BilletesDe100 + billetera02.BilletesDe100,
+        BilletesDe200 = billetera01.BilletesDe200 + billetera02.BilletesDe200,
+        BilletesDe500 = billetera01.BilletesDe500 + billetera02.BilletesDe500,
+        BilletesDe1000 = billetera01.BilletesDe1000 + billetera02.BilletesDe1000,
+    };
+    return billeteraNueva;
+}
+
+var billetera3 = combinar(billetera1, billetera2);
+
+billetera1.LimpiarBilletera();
+billetera2.LimpiarBilletera();
+
+Console.WriteLine("");
+Console.WriteLine("--------Después de limpiar las billeteras--------");
+
+//valores finales de la billetera1.
+Console.WriteLine($"En la billetera 1 hay: {billetera1.TotalEnBilletera()} pesos");
+
+//valores finales de la billetera2.
+Console.WriteLine($"En la billetera 2 hay: {billetera2.TotalEnBilletera()} pesos");
+
+//Valores combinados de ambas billeteras en la billetera3.
+Console.WriteLine($"En la billetera 3 hay: {billetera3.TotalEnBilletera()} pesos");
